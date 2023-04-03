5f937e077092124b90428eb15ca370969cf3c253
Yashar PourMohammad <yasharpm@users.noreply.github.com>
2023-04-03 21:05:18 +0000

    Removed unneeded apps that were added to the core

--- apps/openidconnect/l10n/bg_BG.js
+++
@@ -1,6 +0,0 @@
-OC.L10N.register(
-    "openidconnect",
-    {
-    "Only guests are allowed through this authentication mechanism" : "Чрез този механизъм за удостоверяване се допускат само гости"
-},
-"nplurals=2; plural=(n != 1);");
--- apps/openidconnect/l10n/tr.js
+++
@@ -1,6 +0,0 @@
-OC.L10N.register(
-    "openidconnect",
-    {
-    "Only guests are allowed through this authentication mechanism" : "Bu kimlik doğrulama mekanizması aracılığıyla yalnızca misafirlere izin verilir"
-},
-"nplurals=2; plural=(n > 1);");
