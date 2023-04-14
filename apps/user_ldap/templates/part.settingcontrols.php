4392d42420118a6a10471bff50b2865a9fd8c7cf
navid-shokri <123634558+navid-shokri@users.noreply.github.com>
2023-04-14 08:40:45 +0000

    Revert "Merge pull request #7 from pondersource/feat/oidc-ldap"
    This reverts commit b4ba49d76279cadc252c5c9ee50a36ef20b6ef78, reversing
    changes made to 0d7441422741142a248fe6184794c81c5abed06a.

--- apps/user_ldap/templates/part.settingcontrols.php
+++
@@ -1,5 +0,0 @@
-<div class="ldapSettingControls">
-	<button type="button" class="ldap_action_test_connection" name="ldap_action_test_connection">
-		<?php p($l->t('Test Configuration'));?>
-	</button>
-</div>
